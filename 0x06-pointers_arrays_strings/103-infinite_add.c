a71ccb56c3d7a4d362edc64df45087c222d1da1f
Junaid Zainab <junaidhorlameedah@gmail.com>
2022-12-23 09:37:01 -0800

    new task

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -6,7 +6,6 @@
  * @n2: second number
  * @r: buffer for result
  * @size_r: buffer size
- * ahhh! Crazy task! Expand your knowledge
  * Return: address of r or 0
  */
 
@@ -19,10 +18,8 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	for (j = 0; n2[j]; j++)
 		;
 	if (i > size_r || j > size_r)
-		;
-	return (0);
+		return (0);
 	m = 0;
-
 	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
 	{
 		n = m;
